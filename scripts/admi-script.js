8a1cfc41f62194e6dcc658b4ece9ad95362b2191
Hoj80r <hojjat80r@gmail.com>
2023-08-23 10:13:17 +0330

    add project file to git

---
+++ scripts/admi-script.js
@@ -0,0 +1,10 @@
+// sidebar collapsing
+$(document).ready(function(){
+    $('#user-btn,#pod-btn,#ask-btn,#blog-btn,#web-btn').click(function(){
+      $('.collap-data').removeClass('show')
+    });
+  });
+
+  // upload file
+  $('.file-upload').file_upload();
+  
