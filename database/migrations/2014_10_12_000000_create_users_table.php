9af78162948e2a952b143627758ce6ed5c12abe7
Amir <rasti2001@yahoo.com>
2023-09-10 23:08:58 +0330

    start-point

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
+            $table->string('lastName')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->char('mobile', 11)->nullable();
+            $table->enum('role', ['admin', 'user']);
+            $table->string('password');  
             $table->rememberToken();
             $table->timestamps();
         });
