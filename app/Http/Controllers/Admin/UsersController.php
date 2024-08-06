b5d0e264d6e6af4e66e544e11454c8ed0c192638
Amir <rasti2001@yahoo.com>
2024-08-06 23:44:55 +0330

    creating-users-incomplete

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Users\StoreRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -15,4 +17,22 @@ class UsersController extends Controller
 
         return view('admin.users.add');
     }
+    public function store(StoreRequest $request){
+        $validatedData = $request->validated();
+
+        $createdUser = User::create([
+              'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'mobile' => $validatedData['mobile'],
+            'password' => $validatedData['password'],
+            'role' => $validatedData['role'],
+            'profile_url' => $validatedData['profile_url']
+        ]);
+        if (!$createdUser){
+            return back()->with('failed', 'کابر ایجاد نشد');
+        }
+
+        return back()->with('success', 'کاربر ایجاد شد');
+    }
 }
+
--- resources/views/admin/users/add.blade.php
+++ resources/views/admin/users/add.blade.php
@@ -10,12 +10,14 @@
         </nav>
 
         <div class="main-list col-9 float-start pe-5 ps-5 ">
-            <form action="">
+            @include('errors.message')
+            <form action=" {{ route('admin.users.store') }}">
+                @csrf
                 <div class="d-flex justify-content-between mt-5">
                     <!-- first & lastname fild -->
                     <div class="col-4">
-                        <input class="form-control m-3" type="text" placeholder="نام و نام خانوادگی">
-                        <input class="form-control m-3 text-start" type="tel" placeholder="09xxxxxxxxx">
+                        <input class="form-control m-3" type="text" name="name" placeholder="نام و نام خانوادگی">
+                        <input class="form-control m-3 text-start" type="tel" name="mobile" placeholder="09xxxxxxxxx">
                     </div>
                     <!-- image upload input -->
                     <div class="col-4">
@@ -27,15 +29,26 @@
                         <div class="d-flex justify-content-center">
                             <div class="btn btn-secondary" id="add-userImgBtn">
                                 <label class="form-label text-white m-1" for="customFile2">آپلود عکس</label>
-                                <input type="file" class="form-control d-none" id="customFile2" accept="image/*" />
+                                <input type="file" name="profile_url" class="form-control d-none" id="customFile2" accept="image/*" />
                             </div>
                         </div>
                     </div>
                     <!-- e-mail & phone-number fild -->
                     <div class="col-4">
-                        <input class="form-control m-3" type="password" name="phone" dir="ltr" placeholder="password">
-                        <input type="email" class="form-control m-3" dir="ltr" id="exampleFormControlInput1"
+                        <input class="form-control m-3" type="password" name="password" dir="ltr" placeholder="password">
+                        <input type="email" name="email" class="form-control m-3" dir="ltr" id="exampleFormControlInput1"
                                placeholder="name@example.com">
+                    </div>
+                </div>
+                <!-- role -->
+                <div class="col-md-4">
+                    <div class="form-group">
+                       {{-- <label>نقش کاربری</label>--}}
+                        <select class="form-control" name="role">
+                            <option value="user">کاربر</option>
+                            {{--   <option value="2">طراح و فروشنده</option>--}}
+                            <option value="admin">ادمین</option>
+                        </select>
                     </div>
                 </div>
                 <!-- sub btn -->
