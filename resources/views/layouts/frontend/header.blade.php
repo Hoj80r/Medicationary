cb302496712e72436033a9e0fe90ba44ec5fa16d
Hoj80r <hojjat80r@gmail.com>
2024-08-15 20:17:42 +0330

    nothing

--- resources/views/layouts/frontend/header.blade.php
+++ resources/views/layouts/frontend/header.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/style.css">
     <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.7/dist/iconify-icon.min.js"></script>
-    <script src="../js/podcasts.js"></script>
+    <script type="module" src="../js/podcasts.js"></script>
 </head>
 <body>
     <!-- nav bar -->
