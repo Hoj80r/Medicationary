67afcca1f874c9b9cd47635cea4d97ff5552e6ad
Hoj80r <hojjat80r@gmail.com>
2024-08-22 23:45:43 +0330

    add webinar player section

--- resources/views/frontend/webinarPlayer.blade.php
+++ resources/views/frontend/webinarPlayer.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
 
+<div class="container">
+    <div id="webinarPlayer" class="mt-2 object-fit-fill">
+        <video src="../webinar-videos/videoSample.mp4" controls class="w-100"></video>
+    </div>
 
+</div>
 
 @endsection
