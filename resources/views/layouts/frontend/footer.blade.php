4f237818414eb1793233603303ba6f76354f40ec
Hoj80r <hojjat80r@gmail.com>
2024-08-22 23:10:39 +0330

    change footer desing

--- resources/views/layouts/frontend/footer.blade.php
+++ resources/views/layouts/frontend/footer.blade.php
@@ -1,16 +1,16 @@
     <!-- footer -->
-  <footer class="text-center text-white" id="footer">
+  <footer class="text-white w-100 d-flex flex-wrap" id="footer">
     <!-- Grid container -->
-    <div class="container p-4 pb-0">
+
       <!-- Section: Social media -->
-      <section class="mb-4">
-       
+      <section class="col-12 d-flex justify-content-center p-2">
+
         <a class="btn" href="about-us.html" role="button" title="درباره ما">
-          
-            <iconify-icon icon="mdi:about-circle-outline"></iconify-icon> 
-   
+
+            <iconify-icon icon="mdi:about-circle-outline"></iconify-icon>
+
         </a>
-  
+
         <a class="btn" href="rules.html" role="button">
           قوانین و مقررات
         </a>
@@ -18,20 +18,20 @@
         <a class="btn" href="contact-us.html" role="button">
           <iconify-icon icon="ic:baseline-telegram"></iconify-icon>
         </a>
-        
+
       </section>
       <!-- Section: Social media -->
-    </div>
+
     <!-- Grid container -->
-  
+
     <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    <div class="text-center col-12 " style="background-color: rgba(0, 0, 0, 0.2);">
       تمامی حقوق برای <p class="display-4 d-inline">...</p> محفوظ است.
     </div>
     <!-- Copyright -->
   </footer>
-  
-    
+
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
 </body>
