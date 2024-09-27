458a942a35e0be90b4f366551cda85feff8802fb
Hoj80r <hojjat80r@gmail.com>
2024-09-27 12:37:11 +0330

    fix the code of site page paginations

--- resources/views/frontend/webinar.blade.php
+++ resources/views/frontend/webinar.blade.php
@@ -34,17 +34,9 @@
       @endforeach
     </div>
     {{-- pagination --}}
-   <nav aria-label="Page navigation example" dir="ltr">
-    <ul class="pagination justify-content-center">
-      <li class="page-item disabled">
-        <a class="page-link">Previous</a>
-      </li>
-      <li class="page-item"><a class="page-link" href="#">1</a></li>
-      <li class="page-item"><a class="page-link" href="#">2</a></li>
-      <li class="page-item"><a class="page-link" href="#">3</a></li>
-      <li class="page-item">
-        <a class="page-link" href="#">Next</a>
-      </li>
-    </ul>
-  </nav>
+    <nav aria-label="Page navigation example" dir="ltr">
+        <ul class="pagination justify-content-center">
+            {{$posts->links()}}
+        </ul>
+    </nav>
 @endsection
