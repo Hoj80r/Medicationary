e2adf35d734c70abbc49d53a96a5c7056255ec6c
Hoj80r <hojjat80r@gmail.com>
2024-09-27 12:29:35 +0330

    add pagination to website pages

--- resources/views/frontend/answers.blade.php
+++ resources/views/frontend/answers.blade.php
@@ -29,6 +29,20 @@
           <a href="{{route('home.post', $response->id)}}"><button class="article-btn btn">مطالعه بیشتر</button></a>
         </div>
       </div>
-  </div>
+
   @endforeach
+  {{-- pagination --}}
+  <nav aria-label="Page navigation example" dir="ltr">
+    <ul class="pagination justify-content-center">
+      <li class="page-item disabled">
+        <a class="page-link">Previous</a>
+      </li>
+      <li class="page-item"><a class="page-link" href="#">1</a></li>
+      <li class="page-item"><a class="page-link" href="#">2</a></li>
+      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      <li class="page-item">
+        <a class="page-link" href="#">Next</a>
+      </li>
+    </ul>
+  </nav>
 @endsection
